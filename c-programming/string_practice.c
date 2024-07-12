20f8eb99d6c5b796190dbb46b401a0ed97615230
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-07-13 01:33:55 +0530

    commit

---
+++ c-programming/string_practice.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char a[]="hello world";
+    
+    for (int i=0;i<strlen(a);i++)
+    printf("%c",a[i]);
+
+
+    printf("Enter the string \n");
+    printf("Enter the length of string: ");
+
+}

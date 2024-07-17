09b93602ad972cf2c22c5161c55fa52b00c9bd45
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-07-18 00:34:52 +0530

    commit

---
+++ c-programming/2d_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+    int arr[2][3]={{1,2,3},{4,9,6}};
+    for (int i = 0; i <2; i++)
+    {
+        for (int j = 0; j <3; j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+        
+    }
+int *ptr=arr[0];
+for (int i = 0; i < 6; i++)
+{
+    /* code */
+printf("%d\t",*ptr);   
+ptr++; 
+}
+
+}

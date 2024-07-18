e825b2c08612fe605f5ef4436d866824a5ddf473
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-07-19 00:32:03 +0530

    basic code for single link list

--- DSA/link_list.c
+++ DSA/link_list.c
@@ -1,23 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 typedef struct node
 {
     int data;
-    struct node* next;
-}node;
-void main(){
+    struct node *next;
+} node;
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != 0)
+    {
+        printf("%d\t", temp->data);
+        temp = temp->next;
+    }
+}
+node *ins_atLast(node *head)
+{
+    node *temp = head, *new_node;
+    new_node = (node *)malloc(sizeof(node));
     int num;
-    node * head=0;
-    node * new_node,*temp;
-    new_node=(node*)malloc(sizeof(node));
     printf("Enter the number: ");
-    scanf("%d",&num);
+    scanf("%d", &num);
+    new_node->next=0;
     new_node->data=num;
-    if (head==0){
-        head=temp=new_node;
+    if (head == 0)
+    {
+        head = new_node;
     }
-    else{
-        temp->next=new_node;
-        temp=new_node;
+    else
+    {
+
+        while (temp->next != 0)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
     }
+    return head;
+}
+void main()
+{
+    
+    node *head = 0;
+    head=ins_atLast(head);
+    head=ins_atLast(head);
+    head=ins_atLast(head);
+    display(head);
 }

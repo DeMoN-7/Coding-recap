d4580402eac38a261a9621f990706fdc04591bdd
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-07-22 15:41:23 +0530

    commit

--- DSA/Final_Single_Link_list_optimized_code.c
+++ DSA/Final_Single_Link_list_optimized_code.c
@@ -28,10 +28,27 @@ void display(node *head)
             printf("%d\t", temp->data);
             temp = temp->next;
         }
-            printf("%d\t", temp->data);
-
+        printf("%d\t", temp->data);
     }
     printf("\n");
+}
+int size(node *head)
+{
+    int count = 1;
+    if (head == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        node *temp = head;
+        while (temp->next != 0)
+        {
+            count++;
+            temp = temp->next;
+        }
+    }
+    return count;
 }
 node *insert(node *head)
 {
@@ -51,12 +68,13 @@ node *insert(node *head)
         {
             temp = temp->next;
         }
-        temp->next= new_node;
+        temp->next = new_node;
     }
     return head;
 }
-node* insert_atLast(node * head){
-    node* temp=head,*new_node;
+node *insert_atLast(node *head)
+{
+    node *temp = head, *new_node;
     new_node = memory();
     int num;
     printf("Enter the number: ");
@@ -72,30 +90,63 @@ node* insert_atLast(node * head){
         {
             temp = temp->next;
         }
-        temp->next= new_node;
+        temp->next = new_node;
     }
     return head;
 }
-node* ins_first(node * head){
-    node * temp=head,*new_node;
+node *ins_first(node *head)
+{
+    node *temp = head, *new_node;
     int num;
     printf("Enter the number: ");
     scanf("%d", &num);
-    new_node=memory();
-    new_node->next=temp;
-    new_node->data=num;
+    new_node = memory();
+    new_node->next = temp;
+    new_node->data = num;
     // head=new_node;
     return new_node;
+}
+node *ins_at_pos(node *head)
+{
+
+    node *temp = head, *temp2, *new_node;
+    int num, pos;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    int siz = size(head);
+    printf("Enter the positon you want to enter: ");
+    scanf("%d", &pos);
+    if (pos > siz)
+    {
+        printf("\nYou are trying to access an out-of-range element.\n");
+    }
+    else
+    {
+        new_node = memory();
+        new_node->data = num;
+        for (int i = 1; i < pos-1; i++)
+        {
+            temp = temp->next;
+        }
+        temp2 = temp->next;
+        temp->next = new_node;
+        new_node->next = temp2;
+    }
+
+    return head;
+}
+node * ins_bef_pos(node * head){
     
 }
 void main()
 {
 
     int choice;
+    int count = 0;
     node *head = 0;
     do
     {
-        printf("\nPress 0 for exit\nPress 1 for insert\n Press 2 for insert at last \n Press 3 for insert at first \n Press 4 for insert at position \n Press 5 for insert after position \nPress 6 insert before postion\n Press 7 for Deleting last node \n Press 8 Delete All\n Press 9 for deleting certain nodes\n Press 10 for Displaying all nodes ");
+        printf("\nPress 0 for exit\nPress 1 for insert\n Press 2 for insert at last \n Press 3 for insert at first \n Press 4 for insert at position \n Press 5 for insert after position \nPress 6 insert before postion\n Press 7 for Deleting last node \n Press 8 Delete All\n Press 9 for deleting certain nodes\n Press 10 for Displaying all nodes\n Press 11 for Counting the nodes  ");
         scanf("%d", &choice);
         switch (choice)
         {
@@ -103,13 +154,20 @@ void main()
             head = insert(head);
             break;
         case 2:
-            head= insert_atLast(head);
+            head = insert_atLast(head);
             break;
         case 3:
-            head=ins_first(head);
+            head = ins_first(head);
+            break;
+        case 4:
+            head = ins_at_pos(head);
             break;
         case 10:
             display(head);
+            break;
+        case 11:
+            count = size(head);
+            printf("\nThe size of the linked list is : %d\n", count);
             break;
         default:
             break;

23695c5c4bf5ac277d3e7824a66dbd0a1eb376b8
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-08-04 03:21:52 +0530

    Java Revision

---
+++ Java/Rev1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Rev1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the int");
+        int ar=sc.nextInt();
+        System.out.println("wwe"+ar);
+        
+    }
+}

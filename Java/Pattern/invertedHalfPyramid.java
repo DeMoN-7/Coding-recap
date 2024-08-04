25a99e2248de1f0ed785eabb55a0de48f3c77b0c
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-08-05 00:20:05 +0530

    inverted half pyramid

---
+++ Java/Pattern/invertedHalfPyramid.java
@@ -0,0 +1,11 @@
+public class invertedHalfPyramid {
+    public static void main(String[] args) {
+        System.out.println("Pattern: ");
+        for(int i=1;i<=5;i++){
+            for(int j=i;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

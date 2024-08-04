3207bae24edd91f73754cbccd3f304ced00b54ba
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-08-05 01:49:17 +0530

    inverted half pyramid with number

---
+++ Java/Pattern/invertedHalfPyramidWithNumber.java
@@ -0,0 +1,11 @@
+public class invertedHalfPyramidWithNumber {
+    public static void main(String[] args) {
+        System.out.println("Pattern: ");
+        for(int i=5;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}

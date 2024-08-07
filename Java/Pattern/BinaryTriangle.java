7bcc54d9de79a0009bf5544921194e726ff67808
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-08-08 01:22:33 +0530

    updated code

--- Java/Pattern/BinaryTriangle.java
+++ Java/Pattern/BinaryTriangle.java
@@ -8,5 +8,16 @@
 public class BinaryTriangle {
     public static void main(String[] args) {
         System.out.println("Pattern: ");
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+            if(i%2==0 && j%2==0 || i%2!=0 && j%2!=0){
+                System.out.print("1");
+            }
+            else{
+                System.out.print("0");
+            }
+            }
+            System.out.println();
+        }
     }
 }

1f898978ffe94c73a2c8883df02a5854be86216f
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-08-24 09:28:58 +0530

    basic function

---
+++ Java/function/fun2.java
@@ -0,0 +1,10 @@
+public class fun2 {
+    public static int GivSum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Main");
+        int sum=GivSum(2,3);
+        System.out.println("sum: "+ sum);
+    }
+}

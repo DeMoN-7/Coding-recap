ef29d72823fb1615a6155615821e46d82da70f83
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-02 01:04:45 +0530

    string builder

--- Java/Strings/StringBUilder.java
+++ Java/Strings/StringBUilder.java
@@ -2,5 +2,15 @@ public class StringBUilder {
     public static void main(String[] args) {
         StringBuilder sb= new StringBuilder("hello");
         System.out.println(sb);
+        // change character at 
+        sb.setCharAt(0,'W');
+        System.out.println(sb);
+        // insert 
+        System.out.println(sb.insert(0, "Sh"));
+        System.out.println(sb);
+        // Delete element
+        sb.delete(2, 4);
+        System.out.println(sb);
+        sb.append(22);
     }
 }

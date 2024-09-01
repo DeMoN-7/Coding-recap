a61357b8b1d8d2c738abf000a4b1c6c720a87bd6
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-01 16:55:42 +0530

    Strings Basics

---
+++ Java/Strings/String1.java
@@ -0,0 +1,14 @@
+public class String1 {
+public static void main(String[] args) {
+    String fname="Ayush";
+    String lname="Singh";
+    String temp="SsSingh";
+    System.out.println(fname+lname);
+    System.out.println(fname+" "+lname);
+    System.out.println("length: "+fname.length());
+    System.out.println("CharAt: "+fname.charAt(2));
+    System.out.println("substring: "+fname.substring(2));
+    System.out.println("substring: "+fname.repeat(3));
+    // repeat the string n number of counts
+}    
+}

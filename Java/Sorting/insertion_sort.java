b38b2cb2d749c686c7b6db83c620bcc6c95c4388
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-05 03:34:43 +0530

    insertion_sort completed

--- Java/Sorting/insertion_sort.java
+++ Java/Sorting/insertion_sort.java
@@ -1,6 +1,24 @@
 
 public class insertion_sort {
+    public static void printArray(int []arr){
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         System.out.println("Insertion sort");
+        int []arr={7,8,3,1,2};
+        for(int i=0;i<arr.length;i++){
+            int current=arr[i];
+            int j=i-1;
+            while (j>=0 && current<arr[j]) {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+
+        }
+        printArray(arr);
     }
 }

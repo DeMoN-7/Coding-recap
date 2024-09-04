18d6a236064bfdcb7e7b0be5362255fe5bd37ce0
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-05 02:39:54 +0530

    sorting practice

---
+++ Java/Sorting/Selection_sort.java
@@ -0,0 +1,6 @@
+public class Selection_sort {
+    public static void main(String[] args) {
+        System.out.println("Selection sort: ");
+        
+    }
+}
--- Java/Sorting/insertion_sort.java
+++ Java/Sorting/insertion_sort.java
@@ -1,6 +1,6 @@
 
 public class insertion_sort {
     public static void main(String[] args) {
-        
+        System.out.println("Insertion sort");
     }
 }

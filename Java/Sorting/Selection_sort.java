d0482ba3b40e0b2d5d36deff27b97d431866d69c
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-05 02:58:00 +0530

    selection sort code completed

--- Java/Sorting/Selection_sort.java
+++ Java/Sorting/Selection_sort.java
@@ -1,6 +1,33 @@
 public class Selection_sort {
+    public static void printArray(int []arr){
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static int []sort(int [] arr){
+        for (int i=0;i<arr.length;i++){
+            int key_indx=i;
+            
+            for (int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[key_indx]){
+                    key_indx=j;
+                    
+                }
+            }
+            int temp=arr[i];
+                arr[i]=arr[key_indx];
+                arr[key_indx]=temp;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         System.out.println("Selection sort: ");
-        
+        int[] arr={7,8,33,12,1};
+        System.out.println("Before sorting: ");
+        printArray(arr);
+        int[]z=sort(arr);
+        System.out.println("After sorting");
+        printArray(z);
     }
 }

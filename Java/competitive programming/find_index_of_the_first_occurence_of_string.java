ede78c3fb9552400f5627661de9dd12970238a1f
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-10 02:33:37 +0530

    question 28 leetcode

---
+++ Java/competitive programming/find_index_of_the_first_occurence_of_string.java
@@ -0,0 +1,11 @@
+
+public class find_index_of_the_first_occurence_of_string {
+    public static void main(String[] args) {
+
+        String haystack = "sadbutsad";
+        String needle = "sad";
+
+        System.out.println(haystack.indexOf(needle));
+    }
+
+}

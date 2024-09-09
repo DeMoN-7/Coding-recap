30aeb61878855d81f048d8a8b13bebc3d51c14ad
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-09 23:23:09 +0530

    update of code

--- Java/bss.java
+++ Java/bss.java
@@ -1,5 +1,9 @@
 public class bss {
     public static void main(String[] args) {
         System.out.println("12"+12+"12");
+        System.out.println("12"+12+"12");
+        System.out.println("12"+12+"12");
+        System.out.println("12"+12+"12");
+
     }
 }

143b9615f94829523807d9aaf52bb2b99eca78cd
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-13 00:58:33 +0530

    finally solved after 5 failed submission

--- Java/competitive programming/plus_one.java
+++ Java/competitive programming/plus_one.java
@@ -1,5 +1,29 @@
 public class plus_one {
+    static int []plus_on(int []digits){
+        int n = digits.length;
+        digits[n - 1] += 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] == 10) {
+                digits[i] = 0;
+                if (i > 0) {
+                    digits[i - 1] += 1;
+                } else {
+                    int[] newDigits = new int[n + 1];
+                    newDigits[0] = 1;
+                    return newDigits;
+                }
+            }
+        }
+
+        return digits;
+
+    }
+
     public static void main(String[] args) {
-        
+        int[] a = { 9, 9 };
+        int[] x = plus_on(a);
+        for (int i : x)
+            System.out.println(i);
     }
 }

06e4b913be2813733589d364eae6199cd2a17ece
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-13 23:41:42 +0530

    question 35 leet code can be more optimized using binary search to reduce the time complexity

--- Java/competitive programming/search_insert.java
+++ Java/competitive programming/search_insert.java
@@ -1,8 +1,27 @@
 public class search_insert {
-    static int search_insert(int []nums,int target){
-        
+    static int search_inse(int []nums,int target){
+        int n=nums.length;
+        if(n==0)
+        return 0;
+        else{
+            if(target>nums[n-1]){
+                return n;
+            }
+            int x=0;
+            for(int i:nums){
+                if (target<=i){
+                    return x ;
+                }
+                else{
+                    x++;
+                }
+
+            }
+        }
+        return 0;
     }
     public static void main(String[] args) {
-        
+        int []arr={1,2,3,4,5};
+        System.out.println(search_inse(arr, 6));
     }
 }

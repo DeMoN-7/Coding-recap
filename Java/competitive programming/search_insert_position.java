96a8564d279ffce6d99bc4abef3b0186d065e8bd
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-13 22:50:27 +0530

    question 35 leet code not optimized

---
+++ Java/competitive programming/search_insert_position.java
@@ -0,0 +1,10 @@
+public class search_insert_position {
+    static int index(int []arr){
+        for (int i;i<)
+        return 0;
+    }
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5};
+
+    }
+}

03a5843f8aced004a177fd156ec5424bd275c150
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-14 00:12:46 +0530

    still to complete

---
+++ Java/competitive programming/single_number.java
@@ -0,0 +1,13 @@
+public class single_number {
+    static int numm(int []num){
+        int flag=0;
+        for(int i:num){
+            if()
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int []arr={1,2,2,1,3};
+        System.out.println(numm(arr));
+    }
+}

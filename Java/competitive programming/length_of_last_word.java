02329a2f44075a3d2e68614050e6b7a153e7719d
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-13 15:59:09 +0530

    sol of que 58 leetcode

---
+++ Java/competitive programming/length_of_last_word.java
@@ -0,0 +1,11 @@
+public class length_of_last_word {
+    public static void main(String[] args) {
+        String arr="ayush fsaasd  ";
+        // String []str=arr.split(" ");
+        // int n=str.length;
+        // System.out.println(str[n-1].length());
+        arr=arr.trim();
+        int lastSpace=arr.lastIndexOf(" ");
+        System.out.println(arr.length()-lastSpace-1);
+    }
+}

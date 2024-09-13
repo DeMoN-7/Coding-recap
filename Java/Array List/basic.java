8ffcbf5d0da0cbe5322a1aaaf2db601aa28cea15
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-14 01:37:58 +0530

    commit

---
+++ Java/Array List/basic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class basic {
+public static void main(String[] args) {
+    ArrayList<Integer> list=new ArrayList<>();
+    list.add(0); // to add element on list at last
+    list.add(1); 
+    list.add(2); 
+    list.add(3); 
+    list.add(4);
+    System.out.println(list); // array list can be simply printed without using any loop
+    
+}    
+}
--- Java/competitive programming/single_number.java
+++ Java/competitive programming/single_number.java
@@ -1,13 +1,23 @@
 public class single_number {
     static int numm(int []num){
         int flag=0;
+        int count =0;
         for(int i:num){
-            if()
+            for (int j:num){
+                if(i==j){
+                    flag++;
+                }
+            }
+            if(flag==1){
+                return i;
+            }
+            else
+            flag=0;
         }
         return 0;
     }
     public static void main(String[] args) {
-        int []arr={1,2,2,1,3};
+        int []arr={4,1,2,1,2};
         System.out.println(numm(arr));
     }
 }

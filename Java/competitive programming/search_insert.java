aedcbb9b278af9f5c85660cbd09891302c5929b4
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-13 23:21:24 +0530

    just checking my python knowledge

---
+++ Java/competitive programming/search_insert.java
@@ -0,0 +1,8 @@
+public class search_insert {
+    static int search_insert(int []nums,int target){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ python/comp coding/indexOfFirstOccurence.py
@@ -0,0 +1,5 @@
+def aa(haystack,needle):
+    return haystack.find(needle)
+a="sadadsad"
+b="sad"
+print(aa(a,b))

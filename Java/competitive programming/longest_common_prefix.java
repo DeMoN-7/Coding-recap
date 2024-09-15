6c7b6b94298b6dfec68d536c2e02add79e875178
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-15 23:10:04 +0530

    question 14 leetcode kind of optimized

--- Java/competitive programming/longest_common_prefix.java
+++ Java/competitive programming/longest_common_prefix.java
@@ -1,3 +1,35 @@
 public class longest_common_prefix {
+    static String comm(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
     
+        String fin = strs[0]; 
+        for (int i = 1; i < strs.length; i++) {
+            String current = strs[i];
+    
+           int j = 0;
+            while (j < fin.length() && j < current.length() && fin.charAt(j) == current.charAt(j)) {
+                j++;
+            }
+    
+             fin = fin.substring(0, j);
+    
+            if (fin.isEmpty()) {
+                return "";
+            }
+        }
+    
+        return fin;
+    }
+    
+
+    public static void main(String[] args) {
+        // String[] str = { "flow", "aflower", "flight" };
+        String[] str = { "",""};
+        System.out.println(comm(str));
+    }
 }

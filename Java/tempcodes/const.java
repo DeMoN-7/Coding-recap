c6279d67e43d86f4017a50a456bdf3f89585a4db
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-28 00:48:16 +0530

    just basics

---
+++ Java/tempcodes/const.java
@@ -0,0 +1,5 @@
+public class const{
+    public static void main(String [] args){
+        
+    }
+}
---
+++ Java/tempcodes/const1.java
@@ -0,0 +1,15 @@
+class bike{
+    bike(){
+        System.out.println("cosntructor called");
+    }
+    void prin(){
+        System.out.println("method called");
+    }
+}
+
+public class const1 {
+    public static void main(String[] args) {
+        bike obj= new bike();
+        // obj.prin();
+    }    
+}

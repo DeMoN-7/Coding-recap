de0c0e45ff38115dbc8b876ee5acefd1be3397b3
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-10-02 13:33:46 +0530

    just trying to complete code

--- Java/competitive programming/row_with_max_ones_leetCode.java
+++ Java/competitive programming/row_with_max_ones_leetCode.java
@@ -1,3 +1,11 @@
 public class row_with_max_ones_leetCode {
-    
+    public static int[]row(int [][]mat){
+        for(int i=0;i<row.length;i++)
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = { { 0, 0, 1 }, { 0, 1, 1 }, { 0, 0, 0 } };
+        // System.out.println(row(arr));
+        System.out.println(arr.length);
+    }
 }

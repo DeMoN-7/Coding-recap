0ec5df6f142175e1e44c09138540293a468ff6d0
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-10-21 02:37:08 +0530

    learning

--- Java/Array List/justRevision.java
+++ Java/Array List/justRevision.java
@@ -6,5 +6,12 @@ public class justRevision {
         list.add(21);
         list.add(32);
         System.out.println(list);
+
+        for (int x:list){
+            System.out.println(x*100);
+        }
+
+        System.out.println(list.get(0));
+        System.out.println("Size:"+list.size());
     }
 }

733dab6d0c3fef06f2f01aed5eed4cab55663dfe
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-10-21 02:28:21 +0530

    just quick revison for array list

---
+++ Java/Array List/justRevision.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class justRevision {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(21);
+        list.add(32);
+        System.out.println(list);
+    }
+}

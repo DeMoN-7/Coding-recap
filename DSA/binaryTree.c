a1ef1622a74128014cf826c331f34c0de4d3c473
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-11-26 18:51:26 +0530

    commit

--- DSA/binaryTree.c
+++ DSA/binaryTree.c
@@ -7,6 +7,38 @@ typedef struct  nodetype{
     struct nodetype * right;
 
 }node;
+node * memory(){
+    node * temp;
+    temp= (node*) malloc(sizeof(node));
+    temp->left=0;
+    temp->right=0;
+    return temp;
+}
+node * create(){
+    node * new_node=memory();
+    int n;
+    printf("Enter the data and -1 to exit");
+    scanf("%d",&n);
+    if (n==-1)
+    return NULL;
+    new_node->num=n;
+    printf("Enter left child of %d\n",n);
+    new_node->left=create();    
+    printf("Enter right child of %d\n",n);
+    new_node->right=create();
+    return new_node;
+
+}
+void inorder(node * root){
+    if (root == NULL)
+        return;
+inorder(root->left);
+printf("%d ", root->num);
+inorder(root->right); 
+}
+
 void main(){
-    printf("G");
+    node * root;
+    root=create();
+    inorder(root);
 }

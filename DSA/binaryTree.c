41896a8c00e548e8ac57829ccfa58ee843d6237c
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-11-26 15:06:36 +0530

    commit

---
+++ DSA/binaryTree.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct  nodetype{
+    struct nodetype * left;
+    int num;
+    struct nodetype * right;
+
+}node;
+void main(){
+    printf("G");
+}

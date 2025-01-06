bbdd32f8cc599553fa82def1d878e051ad05c364
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-01-07 00:53:41 +0530

    commit

--- Java/File handling/revision/FileInformation.java
+++ Java/File handling/revision/FileInformation.java
@@ -4,7 +4,11 @@ public class FileInformation {
     public static void main(String[] args) {
         File f=new File("File1.txt");
         if (f.exists()){
-            System.out.println("File exists");
+            System.out.print("Absolute path: ");
+            System.out.println(f.getAbsolutePath());
+            System.out.print("Name: ");
+            System.out.println(f.getName());
+
         }
         else{
             System.out.println("File not found");

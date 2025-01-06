6efcfabb4c8f2667cae98f63c5801a61609dbd38
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-01-07 00:49:35 +0530

    commit

--- Java/File handling/revision/file1.java
+++ Java/File handling/revision/file1.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+
 public class file1 {
 
     public static void main(String[] args) {
-        
+        try{
+            File f=new File("File1.txt");
+            if (f.createNewFile()) {
+                System.out.println("File created");
+            }
+        }catch(IOException e){
+            System.out.println("File not found");
+        }
     }
 }

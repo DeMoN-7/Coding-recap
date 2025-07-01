eadbafdf80639b501819bad31c0b70b078fb2c74
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-07-02 01:37:13 +0530

    gg

---
+++ DSA/Java/Link_list.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.Node;
+
+public class Link_list {
+
+    class node{
+        String data;
+        Node next;
+    }
+    public static void main(String[] args) {
+        
+    }
+}

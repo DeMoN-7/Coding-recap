5f083fc3e94f72e70460e0f104b7407fc19e6c44
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-07-02 01:57:00 +0530

    added add node at last function

--- DSA/Java/Link_list.java
+++ DSA/Java/Link_list.java
@@ -1,39 +1,62 @@
 public class Link_list {
 
     Node head;
-    class Node{
+
+    class Node {
         String data;
         Node next;
-        Node(String data){
-            this.data=data;
-            this.next=null;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
         }
     }
 
-    public void addFirst(String data){
-        Node new_node=new Node(data);
-        if(head==null){
-            head=new_node;
+    public void addFirst(String data) {
+        Node new_node = new Node(data);
+        if (head == null) {
+            head = new_node;
             return;
         }
-        new_node.next=head;
-        head=new_node;
+        new_node.next = head;
+        head = new_node;
     }
-    public void printList(){
-        if (head==null) {
-            System.out.println("List is empty");
+
+    public void addLast(String data) {
+        Node new_node = new Node(data);
+        if (head == null) {
+            head = new_node;
+            return;
         }
-        else{
-            Node temp=head;
-            while(temp!=null){
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = new_node;
+
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+        } else {
+            Node temp = head;
+            while (temp != null) {
                 System.out.println(temp.data);
-                temp=temp.next;
+                temp = temp.next;
             }
         }
     }
 
     public static void main(String[] args) {
-        Link_list list =new Link_list();
+        Link_list list = new Link_list();
+
+        list.addLast("DJ");
+        list.addLast("DJd");
+        list.addLast("DJdd");
+        list.addLast("DJddd");
+        list.printList();
+        System.out.println("...........................");
         list.addFirst("he");
         list.addFirst("hell");
         list.addFirst("hello");

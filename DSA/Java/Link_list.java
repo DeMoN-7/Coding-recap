a91810a7fecfe5dbb5c5eab5132533a5ba5eaa93
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-07-02 01:46:57 +0530

    gg

--- DSA/Java/Link_list.java
+++ DSA/Java/Link_list.java
@@ -1,12 +1,30 @@
-import org.w3c.dom.Node;
-
 public class Link_list {
 
-    class node{
+    Node head;
+    class Node{
         String data;
         Node next;
+        Node(String data){
+            this.data=data;
+            this.next=null;
+        }
     }
+
+    public void addFirst(String data){
+        Node new_node=new Node(data);
+        if(head==null){
+            head=new_node;
+            return;
+        }
+        new_node.next=head;
+        head=new_node;
+    }
+    public void printList()
+
     public static void main(String[] args) {
-        
+        Link_list list =new Link_list();
+        list.addFirst("he");
+        list.addFirst("hell");
+        list.addFirst("hello");
     }
 }

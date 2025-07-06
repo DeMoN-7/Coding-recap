704aa0eb1a621ebad4f3def79121537479a12e51
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-07-06 12:16:00 +0530

    understanding the concept of hash map in java

--- DSA/Java/Hash_Map.java
+++ DSA/Java/Hash_Map.java
@@ -1,3 +1,16 @@
+import java.util.HashMap;
+
 public class Hash_Map {
-    
+    public static void main(String[] args) {
+        HashMap<Integer,String>map=new HashMap<>();
+        // int[]arr={1,2,2,3,3,3};
+        // for(int i=0;i<arr.length;i++)
+        map.put(1, "Ayush");
+        map.put(2, "Siddhant");
+        map.put(3, "Aman");
+        map.put(4, "Bibhav");
+        System.out.println(map);
+        map.put(3, "Mukesh");
+        System.out.println(map);
+    }
 }

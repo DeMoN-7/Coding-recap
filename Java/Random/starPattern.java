4030f94c0405bc034ad71d3bd45d6849221f18d9
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-11-15 02:12:32 +0530

    gg

--- Java/Random/starPattern.java
+++ Java/Random/starPattern.java
@@ -1,3 +1,21 @@
 public class starPattern {
-    
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || i==n-1){
+                    System.out.print("*");
+                }
+                else{
+                    if(j==0 || j==n-1){
+                       System.out.print("*"); 
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
 }
